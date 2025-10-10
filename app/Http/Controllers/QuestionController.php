438b8c06c71826da2c28db6b085382b59bc7bc26
yolandaisabella <yolanda24ti@mahasiswa.pcr.ac.id>
2025-10-10 21:06:07 +0700

    Perubahan

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $data['nama']       = $request->nama;
+        // $data['email']      = $request->email;
+        // $data['pertanyaan'] = $request->pertanyaan;
+
+        // return view('home-question-respon', $data);
+
+        $request->validate([
+            'nama'       => 'required|min:5|max:50',
+            'email'      => 'required|email',
+            'pertanyaan' => 'required|min:10|max:300',
+        ], [
+            'nama.required'         => "Nama Tidah boleh kosong",
+            'email.required'           => "Email Tidak valid",
+            'pertanyaan.required' => "pertanyaan Tidak valid",
+        ]);
+
+        $nama       = $request->input('nama');
+        $email      = $request->input('email');
+        $pertanyaan = $request->input('pertanyaan');
+
+        return view('home-question-respon', compact('nama', 'email', 'pertanyaan'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
