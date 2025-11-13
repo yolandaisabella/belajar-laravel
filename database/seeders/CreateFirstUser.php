f5da0776c6c23877c1b0f9fa06c901a90beb3859
nasywa <nasywa24ti@mahasiswa.pcr.ac.id>
2025-11-13 08:45:23 +0700

    P10

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'gatot@pcr.ac.id',
+            'password' => Hash::make('gatotkaca')
+        ]);
+    }
+}
