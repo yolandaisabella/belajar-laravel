fcecede643b9d2f0701394dfa40d0e7055229942
yolandaisabella <yolanda24ti@mahasiswa.pcr.ac.id>
2025-11-13 11:03:11 +0700

    .

--- database/seeders/CreatePelangganDummy.php
+++ database/seeders/CreatePelangganDummy.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 use Illuminate\Support\Facades\DB;
-
 
 class CreatePelangganDummy extends Seeder
 {
