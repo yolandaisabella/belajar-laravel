cea827fb81c06a98849c1055482da78999a8e53e
yolanda <yolanda24ti@mahasiswa.pcr.ac.id>
2025-11-20 09:29:25 +0700

    .

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -13,7 +13,7 @@ class PelangganController extends Controller
     public function index(Request $request)
     {
         $filterableColumns = ['gender'];
-        $searchableColumns = ['first_name'];
+        $searchableColumns = ['first_name', 'last_name', 'email'];
         $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns) -> search($request,$searchableColumns) -> paginate(10) -> withQueryString();
 		return view('admin.pelanggan.index',$data);
     }
