5c9470e86ea802ac7d176e6a6dc6b893716ec204
yolandaisabella <yolanda24ti@mahasiswa.pcr.ac.id>
2025-12-02 23:45:26 +0700

    P12

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,39 @@ Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show')
 
 // Edit profile
 Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+
+
+//halaman guest
+Route::middleware('guest')->group(function(){
+
+    //Halaman Form Login
+    Route::get('/auth', [AuthController::class,'index'])->name('login');
+
+    //Proses Submit Login
+    Route::post('/auth/login', [AuthController::class,'login'])->name('login.process');
+
+    // Halaman Depan
+    Route::get('/', function(){
+        return view('welcome');
+    });
+});
+
+//halaman wajib login
+Route::middleware('auth')->group(function(){
+
+    // Logout (Bisa diakses semua user yang login)
+    Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+
+    // --- DASHBOARD UNTUK USER BIASA ---
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+
+    // Fitur User Biasa (Contoh: Kirim Pertanyaan)
+    Route::post('question/store', [QuestionController::class, 'store'])-> name('question.store');
+    Route::get('/home', [HomeController::class, 'index']);
+
+    //Khusus admin
+    Route::middleware(['role:admin'])->prefix('admin')->group(function(){
+        Route::resource('user', UserController::class);
+        Route::resource('pelanggan', PelangganControllerController::class);
+    });
+});
